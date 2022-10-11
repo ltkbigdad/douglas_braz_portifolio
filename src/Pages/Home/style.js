f5bc003da1e763ed3b8f1c094973939121973961
Douglas Braz Batista <90732177+ltkbigdad@users.noreply.github.com>
2022-10-10 21:40:24 -0300

    fix: home provisória

--- src/Pages/Home/style.js
+++ src/Pages/Home/style.js
@@ -2,6 +2,13 @@ import styled from "styled-components"
 
 export const Container = styled.div`
     height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img{
+        width: 30%;       
+    }
     
     
     
