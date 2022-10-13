d0166de0fe4a421003a07f3f62362aa99cf119e5
Douglas Braz Batista <90732177+ltkbigdad@users.noreply.github.com>
2022-10-13 18:58:44 -0300

    fix: inicio do modal menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,30 +1,57 @@
-import { Container } from "./style"
+import { Container, MenuContainer } from "./style"
 import {TiThMenu} from "react-icons/ti"
+import { useState } from "react"
 
 export const Header = () => {
+    const [ isMenu, setIsMenu] = useState(false)
+
+    function handleMenu (){
+        if(isMenu === false){
+            return setIsMenu(true)
+        } else{
+            return setIsMenu(false)
+        }
+    }
+
+
     return (
-        <Container>
-            <p className="logo">Portifólio</p>
+        <>        
+        {
+            isMenu ? (
+                <MenuContainer>
+                    <p>teste menu</p>
+                </MenuContainer>
+                 
+    
+            ) : (
+            <Container>
+                <p className="logo">Portifólio</p>
+    
+                <TiThMenu className="menuHamburguer" onClick={()=> handleMenu()}/>
+    
+                <ul className="menu">
+                    <li className="menuLista">
+                        <a>Sobre</a>
+                    </li>
+                    <li className="menuLista">
+                        <a>Especialidades</a>
+                    </li>
+                    <li className="menuLista">
+                        <a>Projetos</a>
+                    </li>
+                    <li className="menuLista">
+                        <a>Contato</a>
+                    </li>
+                </ul>
+            </Container>      
+             
+            )
+    
+            
+        }
+        </>
 
-            <TiThMenu className="menuHamburguer"/>
 
-            <ul className="menu">
-                <li className="menuLista">
-                    <a>Sobre</a>
-                </li>
-                <li className="menuLista">
-                    <a>Especialidades</a>
-                </li>
-                <li className="menuLista">
-                    <a>Projetos</a>
-                </li>
-                <li className="menuLista">
-                    <a>Contato</a>
-                </li>
-            </ul>
-
-
-        </Container>        
 
     )
 }
